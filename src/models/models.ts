b0be44e1ab3064d255916ce7198ce77009692afc
Madhusudan Yadav <myadav1152@gmail.com>
2024-01-14 18:21:08 +0530

    first

---
+++ src/models/models.ts
@@ -0,0 +1,29 @@
+// import React, { useReducer } from 'react'
+export interface Todo {
+    id:number,
+    todo:string,
+    isDone:boolean
+}
+
+// type Action = {type:'add',payload:string}| 
+// {type:'remove',payload:number} | 
+// {type:'done',payload:number};
+
+
+// export const TodoReducer = (state:Todo[],action:Action) => {
+//     switch(action.type){
+//         case "add":
+//             return [
+//                 ...state, {id:Date.now(),todo:action.payload,isDone:false}
+//             ]
+            
+//         case "remove":
+//             return state.filter((todo)=>todo.id!=action.payload)
+//         case "done":
+//             return state.map((todo)=>todo.id===action.payload?{...todo,isDone: !todo.isDone}:todo)
+//         default :
+//         return state;
+//         }
+// }
+
+// export default TodoReducer ;
